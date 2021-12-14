09c8aa84fd29062079c0fd80fec8423a2583a35b
Philippe Busch <philippe.busch@gmail.com>
2021-12-14 09:55:45 +0100

    Factorisation

--- backend/app.js
+++ backend/app.js
@@ -1,35 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
-const sauceRoutes = require('./routes/sauce');
-const path = require('path');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
+const path = require("path");
+require("dotenv").config();
 
-
-mongoose.connect('mongodb://localhost:27017/hottakes', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/hottakes", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log("connecté à Mongoose")
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("connecté à Mongoose");
 });
 
-
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/api/auth', userRoutes);
+app.use("/api/auth", userRoutes);
 
-app.use('/api/sauces', sauceRoutes);
-
-
+app.use("/api/sauces", sauceRoutes);
 
 module.exports = app;
